8a3c0cbf2f457cde21b6d7b2c2cd2385f72bd6a6
Said Lfagrouche <saidlfagrouche@gmail.com>
2025-02-18 21:48:51 -0500

    doubled(hunny_jars)

---
+++ main (3).py
@@ -0,0 +1,10 @@
+def doubled(hunny_jars):
+	for i in range(len(hunny_jars)):
+	    hunny_jars[i] = hunny_jars[i] * 2
+	
+	return hunny_jars
+	
+
+	
+hunny_jars = [1, 2, 4]
+print(doubled(hunny_jars))
