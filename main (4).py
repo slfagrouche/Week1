da9d5bae5b44d3acd783668433c27d7665155469
Said Lfagrouche <saidlfagrouche@gmail.com>
2025-02-18 21:49:40 -0500

    sum_honey

---
+++ main (4).py
@@ -0,0 +1,16 @@
+def sum_honey(hunny_jars):
+	sum_hon = 0
+	for num in hunny_jars:
+	    sum_hon+= num
+	    
+	return sum_hon
+	
+hunny_jars = [2, 3, 4, 5]
+print(sum_honey(hunny_jars))
+hunny_jars = []
+print(sum_honey(hunny_jars))
+
+	
+	    
+	
+	
